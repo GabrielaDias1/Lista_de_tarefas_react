890bd9f2d1a39ebccd1db7513b56cf8cb2789b7c
GabrielaDias1 <gabrieladias539@gmail.com>
2024-05-18 02:38:44 -0300

    atividades lista de tarefas ultilizando react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import ListaDeTarefas from './Components/ListaDeTarefas';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <ListaDeTarefas/>
+    </div>
+  );
+}
+
+export default App;
